3587ee35774cd0621e15cb8633dd7f875a546d29
Guiem Portàtil <guillembagurmoll@gmail.com>
2025-02-13 18:40:11 +0100

    first comit

---
+++ src/js/firestore.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMDYH0ozlhfHAbACCAVWKOyHreUV7SOUw",
+  authDomain: "dwec-instrumets.firebaseapp.com",
+  projectId: "dwec-instrumets",
+  storageBucket: "dwec-instrumets.firebasestorage.app",
+  messagingSenderId: "195276760859",
+  appId: "1:195276760859:web:ef28af0ae7d203aff455be"
+};
+
+const app = initializeApp(firebaseConfig);
